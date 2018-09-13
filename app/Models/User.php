05f9707900d8c1497a3ff2fd9a0a79f7ebaa6201
Misbah Ansori <misbah.ansori24@gmail.com>
2018-09-14 05:42:46 +0700

    install spatie permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiablem, HasRoles;
 
     /**
      * The attributes that are mass assignable.
