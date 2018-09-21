b25a1bf7a26d5b9e2c049a1ebfa6a3067ea7881a
Misbah Ansori <misbah.ansori24@gmail.com>
2018-09-21 20:02:03 +0700

    Add tanggal presenter to Pegawai Model

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Presenters\TanggalPresenter;
 
 class Pegawai extends Model
 {
+    use TanggalPresenter;
     /**
      * The attributes that aren't mass assignable.
      *
@@ -53,48 +55,6 @@ class Pegawai extends Model
         return $this->hasMany(Periode::class)
             ->with('bulan')
             ->orderBy('created_at');
-    }
-
-    /**
-     * Set tanggal lahir berdasarkan format
-     *
-     * @param  string  $value
-     * @return void
-     */
-    public function setTanggalLahirAttribute($value)
-    {
-        $this->attributes['tanggal_lahir'] = date('Y-m-d', strtotime($value));
-    }
-
-    /**
-     * Set tanggal masuk berdasarkan format
-     *
-     * @param  string  $value
-     * @return void
-     */
-    public function setTanggalMasukAttribute($value)
-    {
-        $this->attributes['tanggal_masuk'] = date('Y-m-d', strtotime($value));
-    }
-
-    /**
-     * Get Tanggal Masuk attribute
-     *
-     * @return string
-     */
-    public function getTanggalMasukAttribute()
-    {
-        return Carbon::parse($this->attributes['tanggal_masuk'])->format('d-m-Y');
-    }
-    
-    /**
-     * Get Tanggal Lahir attribute
-     *
-     * @return string
-     */
-    public function getTanggalLahirAttribute()
-    {
-        return Carbon::parse($this->attributes['tanggal_lahir'])->format('d-m-Y');
     }
 
     /**
---
+++ app/Models/Presenters/TanggalPresenter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models\Presenters;
+
+use Carbon\Carbon;
+
+trait TanggalPresenter
+{
+    /**
+     * Set tanggal lahir berdasarkan format
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setTanggalLahirAttribute($value)
+    {
+        $this->attributes['tanggal_lahir'] = date('Y-m-d', strtotime($value));
+    }
+
+    /**
+     * Set tanggal masuk berdasarkan format
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setTanggalMasukAttribute($value)
+    {
+        $this->attributes['tanggal_masuk'] = date('Y-m-d', strtotime($value));
+    }
+
+    /**
+     * Get Tanggal Masuk attribute
+     *
+     * @return string
+     */
+    public function getTanggalMasukAttribute()
+    {
+        return Carbon::parse($this->attributes['tanggal_masuk'])->format('d-m-Y');
+    }
+
+    /**
+     * Get Tanggal Lahir attribute
+     *
+     * @return string
+     */
+    public function getTanggalLahirAttribute()
+    {
+        return Carbon::parse($this->attributes['tanggal_lahir'])->format('d-m-Y');
+    }
+}
