85553e4b7b09c0533ce9228c041d708660ccc3a5
Misbah Ansori <misbah.ansori24@gmail.com>
2018-10-13 18:38:04 +0700

    test update

--- tests/Feature/ReadPegawaiTest.php
+++ tests/Feature/ReadPegawaiTest.php
@@ -10,7 +10,7 @@ class ReadPegawaiTest extends TestCase
     use RefreshDatabase;
     
     /** @test */
-    public function an_unauthenticated_user_can_not_browse_pegawai()
+    public function daftar_pegawai_hanya_bisa_dilihat_user_yg_sudah_login()
     {
         $pegawai = factory('App\Models\Pegawai')->create();
 
@@ -19,15 +19,32 @@ class ReadPegawaiTest extends TestCase
     }
     
     /** @test */
-    public function an_authenticated_user_can_browse_pegawai()
+    public function user_hanya_bisa_mengkakses_pegawai_di_bagiannya_saja()
     {
+        $unit = factory('App\Models\Unit')->create();
+
         $user = factory('App\Models\User')->create();
+        $user->assignRole($unit->id);
         
-        $pegawai = factory('App\Models\Pegawai')->create();
+        $pegawai = factory('App\Models\Pegawai')->create(['unit_id' => $unit->id]);
 
         $this->actingAs($user)
-            ->get('/pegawai')
-            ->assertOk();
+            ->get(route('pegawai.index'))
+            ->assertSee($pegawai->nama);
+    }
+
+    /** @test */
+    public function user_tidak_bisa_melihat_pegawai_yang_bukan_bagiannya()
+    {
+        $unit = factory('App\Models\Unit', 2)->create();
+        $user = factory('App\Models\User')->create();
+        $user->assignRole(1);
+        
+        $pegawai = factory('App\Models\Pegawai')->create(['unit_id' => 2]);
+        
+        $this->actingAs($user)
+            ->get(route('pegawai.show', $pegawai->id))
+            ->assertStatus(403);
     }
 
 }
