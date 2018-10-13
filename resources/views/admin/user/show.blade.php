530052ee4a85d5ee3de9c5e7232e6e6c2d76f52d
Misbah Ansori <misbah.ansori24@gmail.com>
2018-10-13 18:37:48 +0700

    ganti tampilan master user show

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -24,6 +24,8 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="row">
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header bg-white d-flex justify-content-between">
@@ -65,6 +67,8 @@
                     </table>
                 </div>
             </div>
+        </div>
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header bg-white">
                     <h4>Hak Akses</h4>
@@ -93,41 +97,41 @@
                 </div>
             </div>
         </div>
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <form action="{{ route('user.role.store', $user->id) }}" method="POST" class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Tambahkan Group</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <form action="{{ route('user.role.store', $user->id) }}" method="POST" class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Tambahkan Group</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @csrf
+                    <div class="form-group">
+                        <label for="role" class="">Silahkan pilih Group</label>
+                        <select class="form-control select2{{ $errors->has('role') ? ' is-invalid' : '' }}" name="role[]" multiple="multiple">
+                            @foreach ($roles as $role)
+                                <option value="{{ $role->id }}">{{ $role->name }}</option>
+                            @endforeach
+                        </select>
+                        @if ($errors->has('role'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('role') }}</strong>
+                            </span>
+                        @endif
                     </div>
-                    <div class="modal-body">
-                        @csrf
-                        <div class="form-group">
-                            <label for="role" class="">Silahkan pilih Group</label>
-                            <select class="form-control select2{{ $errors->has('role') ? ' is-invalid' : '' }}" name="role[]" multiple="multiple">
-                                @foreach ($roles as $role)
-                                    <option value="{{ $role->id }}">{{ $role->name }}</option>
-                                @endforeach
-                            </select>
-                            @if ($errors->has('role'))
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('role') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                        
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">
-                            <i class="ti ti-save"></i> Simpan
-                        </button>
-                    </div>
-                </form>
-            </div>
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="ti ti-save"></i> Simpan
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
