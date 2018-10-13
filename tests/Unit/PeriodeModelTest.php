1b56e925d7c1310c1ae7a1a3e82e93d863ac76b2
Misbah Ansori <misbah.ansori24@gmail.com>
2018-10-13 20:55:58 +0700

    periode bisa tidak bisa diedit test

--- tests/Unit/PeriodeModelTest.php
+++ tests/Unit/PeriodeModelTest.php
@@ -5,11 +5,20 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-
 class PeriodeModelTest extends TestCase
 {
     use RefreshDatabase;
     
+    public function setUp()
+    {
+        parent::setUp();
+        
+        \DB::table('permissions')->insert([
+            ['name' => 'verif kabag', 'guard_name' => 'web'],
+            ['name' => 'verif wadir', 'guard_name' => 'web'],
+        ]);
+
+    }
     /** @test */
     public function it_has_bulan()
     {
@@ -25,5 +34,49 @@ class PeriodeModelTest extends TestCase
         $pegawai = factory('App\Models\Pegawai')->create(['bagian_id' => $bagian->id]);
 
         $this->assertInstanceOf('App\Models\Bagian', $pegawai->bagian);
+    }
+    
+    /** @test */
+    public function periode_bisa_diedit_oleh_siapapun_selama_belum_di_verifikasi()
+    {
+        $user = factory('App\Models\User')->create();
+        $this->actingAs($user);
+        
+        $periode = factory('App\Models\Periode')->create();
+        
+        $this->assertFalse($periode->tidakBisaDiedit());
+    }
+    /** @test */
+    public function periode_tidak_bisa_diedit_jika_sudah_di_verif_kabag_dan_user_bukan_kabag()
+    {
+        $user = factory('App\Models\User')->create();
+        $this->actingAs($user);
+
+        $periode = factory('App\Models\Periode')->create(['verif_kabag' => 1]);
+
+        $this->assertTrue($periode->tidakBisaDiedit());
+    }
+
+    /** @test */
+    public function periode_tidak_bisa_diedit_jika_sudah_diverif_wadir_dan_user_bukan_wadir()
+    {
+        $user = factory('App\Models\User')->create();
+        $this->actingAs($user);
+
+        $periode = factory('App\Models\Periode')->create(['verif_wadir' => 1]);
+
+        $this->assertTrue($periode->tidakBisaDiedit());
+    }
+
+    /** @test */
+    public function periode_tidak_bisa_diedit_jika_belum_di_verif_kabag_dan_user_adalah_wadir()
+    {
+        $user = factory('App\Models\User')->create();
+        $this->actingAs($user);
+        $user->givePermissionTo('verif wadir');
+
+        $periode = factory('App\Models\Periode')->create();
+
+        $this->assertTrue($periode->tidakBisaDiedit());
     }
 }
