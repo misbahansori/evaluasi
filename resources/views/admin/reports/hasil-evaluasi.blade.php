9a26d7236dd614c19201bd843aefab58ae9ccc1e
Misbah ansori <misbah.ansori24@gmail.com>
2020-01-11 16:00:06 +0700

    update tampilan report

--- resources/views/admin/reports/hasil-evaluasi.blade.php
+++ resources/views/admin/reports/hasil-evaluasi.blade.php
@@ -10,13 +10,13 @@
 </head>
 
 <body>
-    <table class="table">
+    <table class="table table-condensed">
         <tr>
             <td style="width: 25%; height: 64px;">
                 <img src="{{ asset('/img/logo-rsumm.jpg') }}" height="64">
             </td>
-            <td style="width: 50%" class="uppercase text-center leading-relaxed">
-                <h4>Hasil Penilaian Kinerja Pegawai</h4>
+            <td style="width: 50%" class="uppercase text-center leading-snug">
+                <h4>Hasil Penilaian Kinerja Karyawan</h4>
                 <h4>Rsu Muhammadiyah Metro</h4>
                 <h4>Periode {{ $periode->tipe }} {{ $periode->namaBulan }} {{ $periode->tahun }}</h4>
             </td>
@@ -26,31 +26,31 @@
         </tr>
     </table>
     <hr>
-    <table class="table leading-snug">
+    <table class="table leading-snug table-condensed" style="font-size: 13px;">
         <tr>
-            <td>Nama Pegawai</td>
+            <td>Nama Karyawan</td>
             <td>:</td>
-            <td>{{ $periode->pegawai->nama }}</td>
+            <td class="font-bold">{{ $periode->pegawai->nama }}</td>
 
             <td>Unit Kerja</td>
             <td>:</td>
-            <td>{{ $periode->pegawai->unit->name }}</td>
+            <td class="font-bold">{{ $periode->pegawai->unit->name }}</td>
         </tr>
         <tr>
             <td>NBM</td>
             <td>:</td>
-            <td>{{ $periode->pegawai->nbm }}</td>
+            <td class="font-bold">{{ $periode->pegawai->nbm }}</td>
 
             <td>Formasi Tugas</td>
             <td>:</td>
-            <td>{{ $periode->pegawai->formasi->nama }}</td>
+            <td class="font-bold">{{ $periode->pegawai->formasi->nama }}</td>
         </tr>
     </table>
 
-    <h4 style="margin-top: 15px; margin-bottom: 5px;">A. PENILAIAN</h4>
+    <h4>A. PENILAIAN</h4>
 
-    <table class="table table-bordered">
-        <thead>
+    <table class="table table-bordered table-condensed">
+        <thead style="font-size: 12px;">
             <tr>
                 <th rowspan="2" style="vertical-align: middle">No</th>
                 <th rowspan="2" style="vertical-align: middle; text-align: center;">Aspek Penilaian</th>
@@ -95,7 +95,7 @@
             </tr>
             <tr>
                 <td colspan="2">Keterangan</td>
-                <td colspan="5" style="text-align: center; font-weight: bold;">
+                <td colspan="5" style="text-align: center; font-weight: bold; font-size: 12px;">
                     @if ($periode->rataNilai() <= 3)
                         TIDAK DISARANKAN
                     @else
@@ -106,8 +106,8 @@
         </tbody>
     </table>
 
-    <h4 style="margin-top: 15px;">B. KETENTUAN PENILAIAN</h4>
-    <ol style="margin: 0 18px; padding: 0; line-height: 1">
+    <h4 style="margin-top: 10px;">B. KETENTUAN PENILAIAN</h4>
+    <ol style="margin: 0 18px; padding: 0; line-height: 1; font-size: 12px;">
         <li>Nilai rata-rata Al-Islam & Kemuhammadiyahan : <span style="font-family: DejaVu Sans, sans-serif">&lt;</span> 3 TIDAK DISARANKAN</li>
         <li>Nilai rata-rata Al-Islam & Kemuhammadiyahan : <span style="font-family: DejaVu Sans, sans-serif">&ge;</span> 3 DISARANKAN</li>
         <li>Nilai rata-rata Kompetensi : <span style="font-family: DejaVu Sans, sans-serif">&lt;</span> 3 TIDAK DISARANKAN</li>
@@ -117,44 +117,44 @@
 
     @if ($periode->catatan)
         <div style="margin: 10px 0">
-            <h4 style="margin-top: 15px; margin-bottom: 5px;">C. CATATAN</h4>
+            <h4>C. CATATAN</h4>
             <p>{{ $periode->catatan }}</p>
         </div>
     @endif
     @if ($periode->ditingkatkan)
         <div style="margin: 10px 0">
-            <h4 style="margin-top: 15px; margin-bottom: 5px;">D. HAL YANG PERLU DITINGKATKAN</h4>
+            <h4>D. HAL YANG PERLU DITINGKATKAN</h4>
             <p>{{ $periode->ditingkatkan }}</p>
         </div>
     @endif
     @if ($periode->dipertahankan)
         <div style="margin: 10px 0">
-            <h4 style="margin-top: 15px; margin-bottom: 5px;">E. HAL YANG PERLU DIPERTAHANKAN</h4>
+            <h4>E. HAL YANG PERLU DIPERTAHANKAN</h4>
             <p>{{ $periode->dipertahankan }}</p>
         </div>
     @endif
 
     <table class="table">
         <tr>
-            <td class="text-center" style="padding: 15px;">
-                <h4 style="margin-bottom: 50px">PENILAI I</h4>
+            <td class="text-center" style="padding: 10px;">
+                <h4 style="margin-bottom: 40px">PENILAI I</h4>
                 <p>@for ($i = 1; $i < 40; $i++).@endfor</p>
                 <div>NBM : @for ($i = 1; $i < 15; $i++) &nbsp; @endfor</div>
             </td>
-            <td class="text-center" style="padding: 15px;">
-                <h4 style="margin-bottom: 50px">PEGAWAI</h4>
+            <td class="text-center" style="padding: 10px;">
+                <h4 style="margin-bottom: 40px">KARYAWAN</h4>
                 <p class="underline">{{ $periode->pegawai->nama }}</p>
                 <div>NBM : {{ $periode->pegawai->nbm ?? '-' }}</div>
             </td>
         </tr>
         <tr>
-            <td class="text-center" style="padding: 15px;">
-                <h4 style="margin-bottom: 50px">DIREKTUR/WAKIL DIREKTUR</h4>
+            <td class="text-center" style="padding: 10px;">
+                <h4 style="margin-bottom: 40px">DIREKTUR/WAKIL DIREKTUR</h4>
                 <p>@for ($i = 1; $i < 40; $i++).@endfor</p>
                 <div>NBM : @for ($i = 1; $i < 15; $i++) &nbsp; @endfor</div>
             </td>
-            <td class="text-center" style="padding: 15px;">
-                <h4 style="margin-bottom: 50px">PENILAI II</h4>
+            <td class="text-center" style="padding: 10px;">
+                <h4 style="margin-bottom: 40px">PENILAI II</h4>
                 <p>@for ($i = 1; $i < 40; $i++).@endfor</p>
                 <div>NBM : @for ($i = 1; $i < 15; $i++) &nbsp; @endfor</div>
             </td>
